851efeb7f1f5184659e2a02e4ad9aeca2493105d
Shoaib Kamal Khan <shoaibkkhan@hotmail.com>
2020-03-02 18:33:19 -0500

    Updated JS

--- js/app.js
+++ js/app.js
@@ -51,6 +51,8 @@ function addPhraseToDisplay(arr) {
     }
 }
 
+// Call the getRandomPhraseAsArray() function
+
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
 
